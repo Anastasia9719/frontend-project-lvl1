238b23d8e7922c17b03bb7d77a27e7476796a7fd
Nastassia <k.nastassia.97@mail.ru>
2021-05-27 16:51:43 +1000

    bug fix brain-prime

--- bin/src/isNumberPrime.js
+++ bin/src/isNumberPrime.js
@@ -1,13 +1,15 @@
 export const randomNumber = () => Math.floor(Math.random() * 3571) + 1;
 
-// eslint-disable-next-line consistent-return
 export const isNumberPrime = (number) => {
+  let answer = 'yes';
   if (number === 1 || number === 2) {
-    return 'yes';
+    return answer;
   }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      return 'no';
-    } return 'yes';
+      answer = 'no';
+      break;
+    }
   }
+  return answer;
 };
