269a48ebb7afda3e0a67cf52249c156361fc1efb
Nastassia <k.nastassia.97@mail.ru>
2021-06-17 05:03:13 +1000

    delete extra files

--- bin/brain-games.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { greeting, askName } from './src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-
-askName();
-
-greeting();
--- bin/src/askForAnswer.js
+++
@@ -1,5 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const askForAnswer = () => readlineSync.question('Your answer: ');
-
-export default askForAnswer;
