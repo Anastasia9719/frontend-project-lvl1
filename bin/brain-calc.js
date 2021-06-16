d7ed012b3dcabe3edfb5391b85cf530321c29433
Nastassia <k.nastassia.97@mail.ru>
2021-06-17 04:29:22 +1000

    rebuild brain calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,42 +1,5 @@
 #!/usr/bin/env node
-/* eslint-disable no-eval */
 
-import {
-  greeting, askName, congrats, letsTryAgain,
-} from './src/cli.js';
-import randomExpression from './src/randomExpression.js';
-import askForAnswer from './src/askForAnswer.js';
+import startBrainCalc from '../src/games/startBrainCalc.js';
 
-console.log('Welcome to the Brain Games!');
-
-askName();
-greeting();
-
-console.log('What is the result of the expression?');
-
-let countOfCorrectTries = 0;
-
-const numberOfRightTries = 3;
-
-do {
-  const askExpression = randomExpression();
-
-  console.log(`Question: ${askExpression}`);
-
-  const answer = askForAnswer();
-
-  const exit = () => process.exit(-1);
-
-  const solutionOfExpression = () => String(eval(askExpression));
-
-  if (answer === solutionOfExpression()) {
-    console.log('Correct!');
-    countOfCorrectTries += 1;
-  } else {
-    console.log(`'${answer}' is wrong answer :(. Correct answer was '${solutionOfExpression()}'.`);
-    letsTryAgain();
-    exit();
-  }
-} while (countOfCorrectTries < numberOfRightTries);
-
-congrats();
+startBrainCalc();
---
+++ src/games/startBrainCalc.js
@@ -0,0 +1,26 @@
+import gameEngine from '../index.js';
+import randomNumber from '../randomNumber.js';
+
+const gameRules = 'What is the result of the expression?';
+
+const arrayRandomOperator = (operators) => {
+  const randomOperator = (Math.floor(Math.random() * operators.length));
+  return operators[randomOperator];
+};
+
+const operators = ['+', '*', '-'];
+
+const randomExpression = () => `${randomNumber()} ${arrayRandomOperator(operators)} ${randomNumber()}`;
+
+const gameOptions = () => {
+  const question = randomExpression();
+  // eslint-disable-next-line no-eval
+  const rightAnswer = String(eval(question));
+  return [question, rightAnswer];
+};
+
+const startBrainCalc = () => {
+  gameEngine(gameOptions, gameRules);
+};
+
+export default startBrainCalc;
