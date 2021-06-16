2f218c4798eb4c7b070a2f5d785784b399f48ef4
Nastassia <k.nastassia.97@mail.ru>
2021-06-17 04:44:31 +1000

    rebuild brain progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,44 +1,5 @@
 #!/usr/bin/env node
-import askForAnswer from './src/askForAnswer.js';
-import {
-  askName, greeting, congrats, letsTryAgain,
-} from './src/cli.js';
-import arProgression from './src/arProgression.js';
-import findMissNumber from './src/findMissNumber.js';
 
-console.log('Welcome to the Brain Games!');
+import startBrainProgression from '../src/games/startBrainProgression.js';
 
-askName();
-
-greeting();
-
-console.log('What number is missing in the progression?');
-
-let countOfCorrectTries = 0;
-
-const numberOfRightTries = 3;
-
-do {
-  const arifmProgression = arProgression();
-
-  const askProgression = arifmProgression.join(' ');
-
-  console.log(`Question: ${askProgression}`);
-
-  const answer = askForAnswer();
-
-  const exit = () => process.exit(-1);
-
-  const rightAnswer = String(findMissNumber(arifmProgression));
-
-  if (answer === rightAnswer) {
-    console.log('Correct!');
-    countOfCorrectTries += 1;
-  } else {
-    console.log(`'${answer}' is wrong answer :(. Correct answer was '${rightAnswer}'.`);
-    letsTryAgain();
-    exit();
-  }
-} while (countOfCorrectTries < numberOfRightTries);
-
-congrats();
+startBrainProgression();
---
+++ src/games/startBrainProgression.js
@@ -0,0 +1,56 @@
+import gameEngine from '../index.js';
+
+const gameRules = 'What number is missing in the progression?';
+
+const randomNumber10 = () => Math.floor(Math.random() * 10) + 1;
+
+const randomNumber50 = () => Math.floor(Math.random() * 50) + 1;
+
+const arProgression = () => {
+  const array = [];
+  const random10 = randomNumber10();
+  const random50 = randomNumber50();
+
+  for (let i = random50; array.length < 10; i += random10) {
+    array.push(i);
+  }
+  array[random10] = '..';
+  return array;
+};
+
+// eslint-disable-next-line consistent-return
+const findMissNumber = (array) => {
+  const lgth = array.length;
+  const diff1 = array[1] - array[0];
+  const diff2 = array[lgth - 1] - array[lgth - 2];
+  if (array[0] !== '..' && array[1] !== '..') {
+    for (let i = 0; i <= lgth; i += 1) {
+      if (array[i] === '..') {
+        // eslint-disable-next-line no-param-reassign
+        array[i] = array[0] + diff1 * i;
+        return array[i];
+      }
+    }
+  } else {
+    for (let i = 0; i <= lgth; i += 1) {
+      if (array[i] === '..') {
+        // eslint-disable-next-line no-param-reassign
+        array[i] = array[i + 1] - diff2 * i;
+        return array[i];
+      }
+    }
+  }
+};
+
+const gameOptions = () => {
+  const progression = arProgression();
+  const question = progression.join(' ');
+  const rightAnswer = String(findMissNumber(progression));
+  return [question, rightAnswer];
+};
+
+const startBrainProgression = () => {
+  gameEngine(gameOptions, gameRules);
+};
+
+export default startBrainProgression;
