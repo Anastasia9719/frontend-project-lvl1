c3dd5397690c9befe64c2a1c6b690dd2ad0d490d
Nastassia <k.nastassia.97@mail.ru>
2021-06-17 05:01:47 +1000

    src is rised one lvl with bin

---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+let name;
+
+export const askForAnswer = () => readlineSync.question('Your answer: ');
+
+export const askName = () => { name = readlineSync.question('May I have your name?'); };
+
+export const greeting = () => console.log(`Hello, ${name}!`);
+export const noIsWrongAnswer = () => console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+export const yesIsWrongAnswer = () => console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+export const congrats = () => console.log(`Congratulations, ${name}!`);
+export const letsTryAgain = () => console.log(`Let's try again, ${name}!`);
