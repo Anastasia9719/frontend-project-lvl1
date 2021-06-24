eb9456ae8037ae3a545a220c571f090523c04444
Nastassia <k.nastassia.97@mail.ru>
2021-06-24 18:33:39 +1000

    edit brain games and cli

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-import { greeting, askName } from '../src/cli.js';
+import start from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-
-askName();
-
-greeting();
+start();
--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,10 @@
 import readlineSync from 'readline-sync';
 
-let name;
+const start = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
 
-export const askForAnswer = () => readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}!`);
+};
 
-export const askName = () => { name = readlineSync.question('May I have your name?'); };
-
-export const greeting = () => console.log(`Hello, ${name}!`);
-export const noIsWrongAnswer = () => console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
-export const yesIsWrongAnswer = () => console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-export const congrats = () => console.log(`Congratulations, ${name}!`);
-export const letsTryAgain = () => console.log(`Let's try again, ${name}!`);
+export default start;
